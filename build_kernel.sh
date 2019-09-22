9e0cbfb7e7e485b62b38fbe128c0c2861d81d4e3
Tamvans97 <39903107+Tamvans97@users.noreply.github.com>
2019-09-23 03:27:54 +0700

    update: cherry v1.5

--- build_kernel.sh
+++ build_kernel.sh
@@ -18,7 +18,7 @@
 clear
 # Init Fields
 QS_V_FRONT=1
-QS_V_BACK=0
+QS_V_BACK=5
 QS_VERSION=v$QS_V_FRONT.$QS_V_BACK
 QS_VARIANT=grandpplte
 QS_DATE=$(date +%Y%m%d)
@@ -44,3 +44,18 @@ QS_DIR=$(pwd)
 rm -rf $DTS/.*.tmp
 rm -rf $DTS/.*.cmd
 rm -rf $DTS/*.dtb
+#PACK_VARIANT_IMG
+echo "----------------------------------------------"
+	echo "Packing boot.img for $QS_VARIANT..."
+	echo " "
+	mkdir -p $QS_DIR/cherry/tools/aik/ramdisk
+	mkdir -p $QS_DIR/cherry/tools/aik/split_img
+	cp -rf $QS_DIR/cherry/ramdisk/common/ramdisk/* $QS_DIR/cherry/tools/aik/ramdisk
+	cp -rf $QS_DIR/cherry/ramdisk/common/split_img/* $QS_DIR/cherry/tools/aik/split_img
+	mv $QS_DIR/arch/arm/boot/zImage $QS_DIR/cherry/tools/aik/split_img/boot.img-zImage
+	mv $QS_DIR/boot.img-dtb $QS_DIR/cherry/tools/aik/split_img/boot.img-dtb
+	$QS_DIR/cherry/tools/aik/repackimg.sh
+	mv $QS_DIR/cherry/tools/aik/image-new.img $QS_DIR/cherry/build/boot-$QS_VARIANT-$QS_DATE.img
+	$QS_DIR/cherry/tools/aik/cleanup.sh
+echo "----------------------------------------------"
+	echo "Packing boot.img for $QS_VARIANT Finished..."
